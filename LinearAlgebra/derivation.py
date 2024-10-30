4ff16e2cf4ea8a6ecb8f84da42b9e4ec4192e26e
서지원 <yogjesi@naver.com>
2024-10-30 20:36:34 +0900

    ch1_(3)_241030 derivation_end integral_start

--- LinearAlgebra/derivation.py
+++ LinearAlgebra/derivation.py
@@ -50,14 +50,55 @@
 # plot3d(f)
 
 # 1-22
+# from sympy import *
+
+# x, s = symbols('x s')
+# f = x**2
+
+# slope_f = (f.subs(x, x+s)-f) / ((x+s) - x)
+
+# slope_2 = slope_f.subs(x, 2)
+# result = limit(slope_2, s, 0)
+
+# print(result)
+
+# 1-23
+# from sympy import *
+# x, s = symbols('x s')
+# f = x**2
+
+# slope_f = (f.subs(x, x + s) - f) / ((x+s) - x)
+# result = limit(slope_f, s, 0)
+
+# print(result)
+
+# 1-24
+# from sympy import *
+
+# x = symbols('x')
+# z = (x**2 + 1)**3 - 2
+# dz_dx = diff(z, x)
+# print(dz_dx)
+
+# 1-25 연쇄법칙
 from sympy import *
 
-x, s = symbols('x s')
-f = x**2
+x, y = symbols('x y')
 
-slope_f = (f.subs(x, x+s)-f) / ((x+s) - x)
+# 첫 번째 함수의 도함수
+# 충돌을 피하기 위해 _y로 씀
+_y = x**2 + 1
+dy_dx = diff(_y)
 
-slope_2 = slope_f.subs(x, 2)
-result = limit(slope_2, s, 0)
+# 두 번재 도함수
+z = y**3 - 2
+dz_dy = diff(z)
 
-print(result)
+# 연쇄법칙을 사용한 경우와
+# 대체 방식을 사용한 경우 도함수 계산
+dz_dx_chain = (dy_dx * dz_dy).subs(y, _y)
+dz_dx_no_chain= diff(z.subs(y, _y))
+
+# 두 값이 같다는 것을 보임으로써 연쇄 법칙 증명
+print(dz_dx_chain)
+print(dz_dx_no_chain)
